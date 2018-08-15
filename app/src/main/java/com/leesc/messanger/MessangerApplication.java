8885fefa691be9ca6a9dcab9a07c9e3e7fe02315
leesc <slee8789@gmail.com>
2018-08-15 23:22:58 +0900

    default structure design

--- app/src/main/java/com/leesc/messanger/MessangerApplication.java
+++ app/src/main/java/com/leesc/messanger/MessangerApplication.java
@@ -1,14 +1,1 @@
-package com.leesc.messanger;
-
-import com.leesc.messanger.di.DaggerAppComponent;
-
-import dagger.android.AndroidInjector;
-import dagger.android.support.DaggerApplication;
-
-public class MessangerApplication extends DaggerApplication {
-
-    @Override
-    protected AndroidInjector<? extends dagger.android.support.DaggerApplication> applicationInjector() {
-        return DaggerAppComponent.builder().create(this);
-    }
-}
+package com.leesc.messanger;import com.leesc.messanger.di.DaggerAppComponent;import dagger.android.AndroidInjector;import dagger.android.support.DaggerApplication;public class MessangerApplication extends DaggerApplication {    @Override    protected AndroidInjector<? extends dagger.android.support.DaggerApplication> applicationInjector() {        return DaggerAppComponent.builder().create(this);    }}
